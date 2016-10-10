2fbeafdd66dca556e0b4a12c621849e862f239fa
Tyler Prak <tyler.t.prak@gmail.com>
2016-10-10 00:39:32 -0400

    Initial commit

---
+++ JSprog.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+	<head>
+		<title> tyler.prak.com </title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href=style.css" type="text/css">
+		<style type="text/css">
+			
+		</style>
+	</head>
+	<body style="background-color:lightblue">
+		
+		<h1 style="font-size:60px;text-align:center"> Tyler Prak </h1>
+		<p style="text-align:center"> <a href="index.html">Home</a> &emsp; <a href="HTMLprog.html">HTML PROGRESSION </a> &emsp; <a href="JSprog.js">JAVASCRIPT PROGRESSION</a> </p>
+		<hr>
+
+		
+		</body>
+	
+	</html>
